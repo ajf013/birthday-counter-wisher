e2c91da6072efff910d36330b153e7f0ac792f25
ajf013 <jeni13franc@gmail.com>
2021-07-02 10:13:01 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Birthday from './Birthday';
+import { Route, Switch } from 'react-router-dom';
+import RouterBirthday from './RouterBirthday';
+import Generate from './Generate';
+import Footer from './components /Footer/Footer'
+
+function App() {
+  return (
+    <div className='App'>
+      <Switch>
+        <Route exact path='/' component={Birthday} />
+        <Route
+          exact
+          path='/birthday/:name?/:day?/:month?'
+          component={RouterBirthday}
+        />
+        <Route exact path='/generate' component={Generate} />
+      </Switch>
+      <Footer />  
+    </div>
+  );
+}
+
+export default App;
